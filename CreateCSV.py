ac7cd72da1b1f83603ccaffcd844615902ed6efc
Henrique V. Ehrenfried <hvehrenfried@inf.ufpr.br>
2020-09-28 15:40:42 -0300

    Add correct files and basic statistics
    Signed-off-by: Henrique V. Ehrenfried <hvehrenfried@inf.ufpr.br>

--- CreateCSV.py
+++ CreateCSV.py
@@ -12,38 +12,39 @@ if len(sys.argv) < 2:
 
 PATH = sys.argv[1]
 # DATASETS = ["MR","R52"]
-DATASETS = ["MR"]
+DATASETS = ["MR","Ohsumed","R8","R52"]
 
 def pretty_print(df):
     # more options can be specified also
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):  
         print(df) 
 
-def get_metrics(file_path, info):
+def get_metrics(file_path, info, dataset):
+    # epochs = 0
     with open(file_path) as f:
         text = f.readlines()
     for line in text:
+        if line[0:6] == "Epoch:" :
+            epochs = line.split(" ")[1]
         if line[0:17] == "Test set results:":
             tokens = line.split(" ")
             test_result = {
                 "cost":tokens[4], 
                 "accuracy":tokens[6], 
-                "dataset": info[3],
+                "epochs": int(epochs),
+                "dataset": dataset,
                 "experiment": info[1],
                 "name": info[4],
                 "run": info[6].split(".")[0]
                 }
             return (test_result)
 
-def calculate_metrics(df):
-    # TODO: Implment this method
-    # Mean, Variation, Standart Deviation
-    # Student T-test, Friedman test
-    # print(df)
+def calculate_basic_statistics(df, dataset):
+    # Implment Basic statistics and save as csv
     
     all_results = []
     exp = []
-    for i in range(0,25):
+    for i in range(0,19): # This for controls the experiments. Use 25 to get all CUSTOM
         experiment = df.loc[df['experiment'] == i]
         experiment_name = df.loc[df['experiment'] == i]["name"].values[0]
         exp.append(experiment["accuracy"])
@@ -56,35 +57,50 @@ def calculate_metrics(df):
         results["median"] = numpy.median(experiment["accuracy"])
         results["variance"] = numpy.var(experiment["accuracy"])
         results["standard_deviation"] = numpy.std(experiment["accuracy"])
+        results["min_epochs"] = numpy.min(experiment["epochs"])
+        results["max_epochs"] = numpy.max(experiment["epochs"])
+        results["mean_epochs"] = numpy.mean(experiment["epochs"])
+        results["median_epochs"] = numpy.median(experiment["epochs"])
+        results["var_epochs"] = numpy.var(experiment["epochs"])
+        results["std_epochs"] = numpy.std(experiment["epochs"])
         all_results.append(results)
+
+    results = pd.DataFrame(all_results)
+    results.to_csv(
+        "./Basic_Statistics/"+dataset+".csv",
+        sep=';',
+        index=False
+    )
+    return results
     
+
+def calculate_statistics(all_results, dataset):
+
+    print("------ Working with dataset", dataset, "------\n")
+    ORIGINAL_PAPER = {
+        "MR": {"avg": 0.7674, "std": 0.0020},
+        "Ohsumed": {"avg": 0.6836, "std": 0.0056},
+        "R8": {"avg": 0.9707, "std": 0.0010},
+        "R52": {"avg": 0.9356, "std": 0.0018}
+    }
+
     results = pd.DataFrame(all_results)
     max_result = results.loc[(results["mean"] == numpy.max(results["mean"]))]
+    print(results)
+    print(max_result)
 
     ttest_ind_from_stats = stats.ttest_ind_from_stats(
-        mean1= max_result["mean"].values[0],
-        std1= max_result["standard_deviation"].values[0],
-        nobs1= 10,
-        mean2=0.7674,
-        std2=0.002,
-        nobs2=10
-    )
-    ttest_ind_from_stats_inv = stats.ttest_ind_from_stats(
         mean2= max_result["mean"].values[0],
         std2= max_result["standard_deviation"].values[0],
         nobs2= 10,
-        mean1=0.7674,
-        std1=0.002,
+        mean1=ORIGINAL_PAPER[dataset]["avg"],
+        std1=ORIGINAL_PAPER[dataset]["std"],
         nobs1=10
     )
 
+    print(ttest_ind_from_stats)
 
  
-
-
-
-
-
 
 all_results = []
 for dataset in DATASETS:
@@ -95,10 +111,7 @@ for dataset in DATASETS:
         for f in onlyfiles:
             info = f.split("_")
             # print(info)
-            all_results.append(get_metrics(mypath+"/"+f, info))
-
-# for a in all_results:
-#     print(a)
+            all_results.append(get_metrics(mypath+"/"+f, info, dataset))
 
 df = pd.DataFrame(data = all_results)
 
@@ -110,6 +123,10 @@ df["run"] = pd.to_numeric(df["run"])
 df = df.sort_values(by=['experiment','run','dataset'])
 df = df.reset_index(drop=True)
 
-# calculate_metrics(df)
-pretty_print(df)
+for dataset in DATASETS:
+    experiments = df.loc[df['dataset'] == dataset]
+    calculate_basic_statistics(experiments, dataset)
+    
+
+# pretty_print(df)
 
